bbf6abeabc1f56f1b1867de5e62ac5d47b518f4d
unknown <pawelniewiarowski2001@gmail.com>
2025-04-13 16:09:56 +0200

    Change position of background pattern.

--- components/molecules/hero-pattern.vue
+++ components/molecules/hero-pattern.vue
@@ -3,7 +3,7 @@ import pattern from "~/assets/images/patterns/pattern001.png";
 </script>
 
 <template>
-  <div class="hidden xl:block absolute left-110 top-50">
-    <img :src="pattern" width="1000px" alt="background pattern, contains crossing lines"/>
+  <div class="hidden xl:block absolute left-90 top-50">
+    <img :src="pattern" width="1200px" alt="background pattern, contains crossing lines"/>
   </div>
 </template>
