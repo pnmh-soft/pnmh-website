a334317fe34014cb5df896f5bf7d0238660d5ab5
unknown <pawelniewiarowski2001@gmail.com>
2025-04-13 14:11:40 +0200

    Add background pattern for hero section.

---
+++ components/molecules/hero-pattern.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import pattern from "~/assets/images/patterns/pattern001.png";
+</script>
+
+<template>
+  <div class="hidden xl:block absolute left-110 top-50">
+    <img :src="pattern" width="1000px" alt="background pattern, contains crossing lines"/>
+  </div>
+</template>
--- components/organisms/hero.vue
+++ components/organisms/hero.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import AnimatedLogo from "~/components/molecules/animated-logo.vue";
 import Center from "~/components/atoms/helper/center.vue";
+import HeroPattern from "~/components/molecules/hero-pattern.vue";
 </script>
 
 <template>
   <Center>
+    <HeroPattern/>
     <AnimatedLogo/>
   </Center>
 </template>
