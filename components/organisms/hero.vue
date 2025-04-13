fdc551a583efdcfa521638f5704e3da6a1e10036
unknown <pawelniewiarowski2001@gmail.com>
2025-04-13 16:02:45 +0200

    Animate pattern using vanilla css.

--- components/organisms/hero.vue
+++ components/organisms/hero.vue
@@ -2,11 +2,13 @@
 import AnimatedLogo from "~/components/molecules/animated-logo.vue";
 import Center from "~/components/atoms/helper/center.vue";
 import HeroPattern from "~/components/molecules/hero-pattern.vue";
+import HeroAnimatedLine from "~/components/molecules/hero-animated-line.vue";
 </script>
 
 <template>
   <Center>
     <HeroPattern/>
+    <HeroAnimatedLine/>
     <AnimatedLogo/>
   </Center>
 </template>
